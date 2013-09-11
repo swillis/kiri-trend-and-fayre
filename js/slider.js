a88896aff007a1ea87b47901288f7097189b69e6
Sam Willis <sam.willis@event.ly>
2013-09-11 19:42:33 +0100

    initial commit

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+// Can also be used with $(document).ready()
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    itemWidth: 450,
+    itemMargin: 10,
+    controlNav: false,
+    directionNav: false
+  });
+});
